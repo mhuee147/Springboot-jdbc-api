bb27d6d9da37ce310cadfe1b3409584ad265a89e
MinhHue <minhhue14072002@gmail.com>
2022-12-29 06:54:15 +0700

    Data access usinng SELECT

--- src/main/java/com/nbstech/spring/basic/springbootjdbcapi/SpringbootJdbcApiApplication.java
+++ src/main/java/com/nbstech/spring/basic/springbootjdbcapi/SpringbootJdbcApiApplication.java
@@ -1,13 +1,28 @@
 package com.nbstech.spring.basic.springbootjdbcapi;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringbootJdbcApiApplication {
 
+@SpringBootApplication
+public class SpringbootJdbcApiApplication implements CommandLineRunner {
+	private org.slf4j.Logger Logger = (Logger) LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	PlayerDAO playerDao;
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootJdbcApiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Logger.info("All Players Data: {}", playerDao.getAllPlayers());
+
+		Logger.info("Player with Id 3: {}", playerDao.getPlayerById(3));
 	}
 
 }
